ebea51ca8e4efee723372e7b0eeac9eb904c57d6
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2022-11-29 18:26:36 -0300

    2022 11 29 - Initial Modules Commit

---
+++ characters_list/src/main/java/com/clcmo/characters_list/CharactersListActivity.kt
@@ -0,0 +1,11 @@
+package com.clcmo.characters_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharactersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_characters_list)
+    }
+}
---
+++ core/src/main/java/com/clcmo/core/View.kt
@@ -0,0 +1,10 @@
+package com.clcmo.core
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class View : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
